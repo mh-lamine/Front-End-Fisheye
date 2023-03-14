7722eca50330e0edb924f1be4b9cb71034fbce45
mh-lamine <mlamins.ngom@gmail.com>
2023-03-14 19:06:24 +0100

    page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,39 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
+        // const link = document.createElement( 'a' );
+        // link.setAttribute("href", )
+
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const location = document.createElement( 'p' );
+        location.setAttribute("class", "location");
+        location.textContent = `${city}, ${country}`;
+
+        const description = document.createElement( 'p' );
+        description.setAttribute("class", "description");
+        description.textContent = `${tagline}`;
+
+        const pricing = document.createElement( 'p' );
+        pricing.setAttribute("class", "pricing");
+        pricing.textContent = `${price}€/jour`;
+
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(description);
+        article.appendChild(pricing);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
